2266b821219854abc5c66042239a3c7790eeff9a
Stephen Lowrie <stephen.lowrie@gmail.com>
2015-12-08 16:16:54 -0600

    Add common & make tempest calls via stack user

--- stacktools/tempest.py
+++ stacktools/tempest.py
@@ -17,6 +17,9 @@ limitations under the License.
 import argparse
 import logging
 import subprocess
+from pwd import getpwnam
+
+from stacktools.common import demote
 
 LOG = logging.getLogger(__name__)
 
@@ -60,8 +63,13 @@ class RunArgumentParser(argparse.ArgumentParser):
             help="Enables concurrency and specifies worker count", type=int)
 
 
-def initialize_tempest(path):
-    subprocess.call(["testr", "init"], cwd=path)
+def get_stack_user_info():
+    user_info = getpwnam('stack')
+    return (user_info.pw_uid, user_info.pw_gid)
+
+
+def initialize_tempest(path, uid, gid):
+    subprocess.call(["testr", "init"], cwd=path, preexec_fn=demote(uid, gid))
 
 
 def build_run_options(regex, concurrency=None):
@@ -84,24 +92,27 @@ def build_trace_options():
     command.append("-f")
 
 
-def run_piped_commands(cmd_a, cmd_b):
+def run_piped_commands(cmd_a, cmd_b, uid, gid):
     proc_a = subprocess.Popen(
-        cmd_a, stdout=subprocess.PIPE, cwd="/opt/stack/new/tempest")
+        cmd_a, stdout=subprocess.PIPE, cwd="/opt/stack/new/tempest",
+        preexec_fn=demote(uid, gid))
     proc_b = subprocess.Popen(
         cmd_b, stdin=proc_a.stdout, stdout=subprocess.PIPE,
-        cwd="/opt/stack/new/tempest")
+        cwd="/opt/stack/new/tempest", preexec_fn=demote(uid, gid))
     proc_a.stdout.close()
     return proc_b.communicate()[0]
 
 
 def entry_point():
     cl_args = DeployArgumentParser().parse_args()
-    initialize_tempest(cl_args.path)
+    uid, gid = get_stack_user_info()
+    initialize_tempest(cl_args.path, uid, gid)
 
 
 def run_entry_point():
     cl_args = RunArgumentParser().parse_args()
+    uid, gid = get_stack_user_info()
     run_options = build_run_options(
         regex=cl_args.regex, concurrency=cl_args.concurrency)
     trace_options = build_trace_options()
-    run_piped_commands(run_options, trace_options)
+    run_piped_commands(run_options, trace_options, uid, gid)
